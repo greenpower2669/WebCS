158a764358abd4a9d6933c4e2fd14bd735e75271
greenpower2669 <80179295+greenpower2669@users.noreply.github.com>
2026-09-15 20:44:11 +0200

    android: add POC activity shell

---
+++ android/poc-camera/src/main/java/online/tek4all/webcs/poc/MainActivity.kt
@@ -0,0 +1,10 @@
+package online.tek4all.webcs.poc
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
