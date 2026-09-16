fd32ab6bcacf80c7a87fd8cd78f5ff41709cf3b3
greenpower2669 <80179295+greenpower2669@users.noreply.github.com>
2026-09-16 12:17:33 +0200

    Bump WebCS to 0.8.0

--- android/poc-camera/build.gradle.kts
+++ android/poc-camera/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "online.tek4all.webcs.poc"
         minSdk = 26
         targetSdk = 35
-        versionCode = 8
-        versionName = "0.7.0"
+        versionCode = 9
+        versionName = "0.8.0"
     }
 
     compileOptions {
